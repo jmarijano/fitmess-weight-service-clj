028e47b50b238fd8c821d0e08162a29f9f45129d
jmarijano <marijanovicjosip98@gmail.com>
2022-08-04 12:52:34 +0200

    first commit

---
+++ test/fitmess_weight_service_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns fitmess-weight-service-clj.core-test
+  (:require [clojure.test :refer :all]
+            [fitmess-weight-service-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
